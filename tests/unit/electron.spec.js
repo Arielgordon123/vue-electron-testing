1d31f0b8106c9b66b8315fee96b9874e99bc49dc
Ariel Gordon <arielgordon123@gmail.com>
2020-01-15 20:58:02 +0200

    Add time

--- tests/unit/electron.spec.js
+++ tests/unit/electron.spec.js
@@ -1,6 +1,6 @@
 const { testWithSpectron } = require('vue-cli-plugin-electron-builder');
 
-jest.setTimeout(50000);
+jest.setTimeout(100000);
 
 test('Window Loads Properly', async () => {
   // Wait for dev server to start
